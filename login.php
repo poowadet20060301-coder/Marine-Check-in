363e6fa1142773273618a64f6d9172a176f8a1a6
Your Name <your-email@example.com>
2026-04-17 04:06:04 +0700

    Update system

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *'); 
+header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
 // 1. ตั้งค่าการเชื่อมต่อ (เช็คจาก Workbench)
 $host = "localhost";
